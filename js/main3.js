e9ad5c48e2bfc9bb53f86529d1a0a5fcf1393bb4
Grant Sander <gksander93@gmail.com>
2017-05-26 21:02:48 -0700

    Add merged files to master

---
+++ js/main3.js
@@ -0,0 +1,28 @@
+// PREPROS APPENDS
+//@prepros-prepend PG.js
+//@prepros-append listeners.js
+
+// JXG.Options.text.defaultDisplay  = 'internal';
+// JXG.Options.text.display = 'internal';
+
+$(function(){ // On document ready -- Use this to set up web app
+
+    // Make sure localStorage Variables are set up.
+    localStorage['PGconstructions'] = localStorage['PGconstructions'] ? localStorage['PGconstructions'] : "{}";
+    localStorage['PGcurrentConstruction'] = localStorage['PGcurrentConstruction'] ? localStorage['PGcurrentConstruction'] : "0";
+
+    // $("#sidebar").html(sb);
+    jscolor.installByClassName("jscolor");
+
+    PG.loadConstructionList(localStorage['PGcurrentConstruction']);
+    PG.getConstruction(localStorage['PGcurrentConstruction']);
+    PG.loadConstruction(localStorage['PGcurrentConstruction']);
+    PG.showStyles('');
+
+
+}); // End of Document Ready
+
+// Configuration Options for JSXGraph
+JXG.Options.layer = {numlayers: 20, text: 9, point: 9, glider: 9, arc: 8, line: 9, circle: 6,
+          curve: 7, turtle: 5, polygon: 3, sector: 3, angle: 3, integral: 3, axis: 7, ticks: 2, grid: 1, image: 0, trace: 0};
+JXG.Options.text.fontSize = 20;
---
+++ js/packaged.js
@@ -0,0 +1,6 @@
+// @PREPROS-APPEND ../katex/katex.min.js
+// @PREPROS-APPEND auto-render.min.js
+// @PREPROS-APPEND jsxgraphcore.js
+// @PREPROS-APPEND math.min.js
+// @PREPROS-APPEND vue.min.js
+// @PREPROS-APPEND main.js
